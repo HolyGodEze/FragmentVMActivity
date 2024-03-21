7e9538fe41350cb452dcde3ad129479f0853e6c9
Tam Trang <tuo98490@temple.edu>
2024-03-21 11:50:31 -0400

    Added code for the viewModel for clicking the button

--- app/src/main/java/edu/temple/diceroll/DiceFragment.kt
+++ app/src/main/java/edu/temple/diceroll/DiceFragment.kt
@@ -36,7 +36,8 @@ class DiceFragment : Fragment() {
             findViewById<Button>(R.id.rollButton).setOnClickListener {
 
                 // Generate random number
-                (Random.nextInt(sides!!) + 1).toString()
+                val number = Random.nextInt(sides!!) + 1
+                viewModel.setSelectedNumber(number)
             }
         }
     }
