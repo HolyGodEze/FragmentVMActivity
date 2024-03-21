d82c4b1df2053d7b42381e4947bd195e8693d126
Tam Trang <tuo98490@temple.edu>
2024-03-21 11:50:58 -0400

    Added functions for setting and getting the selected number

--- app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
+++ app/src/main/java/edu/temple/diceroll/DiceViewModel.kt
@@ -1,7 +1,19 @@
 package edu.temple.diceroll
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DiceViewModel : ViewModel() {
-    
+    private val selectedNumber = MutableLiveData<Int>()
+
+
+    fun setSelectedNumber(number : Int){
+        selectedNumber.value = number
+    }
+
+    fun getSelectedNumber() : LiveData<Int>{
+        return selectedNumber
+    }
+
 }
